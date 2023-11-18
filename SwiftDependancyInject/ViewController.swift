46ff87c1ecd7b76c2f32943250616e3d27a3bc21
Jinu Joy <jinuparekatil@gmail.com>
2023-11-18 08:06:10 +0530

    Initial Commit

---
+++ SwiftDependancyInject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDependancyInject
+//
+//  Created by Jinu on 18/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
